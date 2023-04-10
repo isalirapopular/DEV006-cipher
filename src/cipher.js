7c33e909469ead7135a30f0b735fa1c30773a1ea
Isabel Lira <isabell.lira@gmail.com>
2023-04-10 11:29:25 -0400

    version terminada de cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,41 @@
 const cipher = {
-  // ...
+  encode: function (Offset, ObtenerInfoMensaje) {
+    if (typeof Number(Offset) !== "number" || typeof ObtenerInfoMensaje !== "string") {
+      throw TypeError("The arguments must be a string and a number.");
+    }
+    let result = "";
+    for (let i = 0; i < ObtenerInfoMensaje.length; i++) {
+      const letterASCII = ObtenerInfoMensaje[i].charCodeAt();
+      if (letterASCII >= 65 && letterASCII <= 90) {
+        result += String.fromCharCode(((letterASCII - 65 + Number(Offset)) % 26) + 65);
+      } else if (letterASCII >= 97 && letterASCII <= 122) {
+        result += String.fromCharCode(((letterASCII - 97 + Number(Offset)) % 26) + 97);
+      } else {
+        result += ObtenerInfoMensaje[i];
+      }
+    }
+    return result;
+  },
+  decode: function (Offset, ObtenerInfoMensaje2) {
+    if (typeof Number(Offset) !== "number" || typeof ObtenerInfoMensaje2 !== "string") {
+      throw TypeError("The arguments must be a string and a number.");
+    }
+    while (Offset > 26) { //indagar sobre esto
+      Offset -= 26;
+      console.log("while")
+    }
+    let result2 = "";
+    for (let i = 0; i <= ObtenerInfoMensaje2.length; i++) {
+      console.log(ObtenerInfoMensaje2)
+      const letterASCII = ObtenerInfoMensaje2.charCodeAt(i);
+      if (letterASCII >= 65 && letterASCII <= 90) {
+        result2 += String.fromCharCode(((letterASCII - 65 - Number(Offset) + 26) % 26) + 65);
+      } else if (letterASCII >= 97 && letterASCII <= 122) {
+        result2 += String.fromCharCode(((letterASCII - 97 - Number(Offset) + 26) % 26) + 97);
+      }
+    }
+    return result2;
+  },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import cipher from './cipher.js';
+// CIFRADO //
+const BtnCifrar = document.getElementById('BtnCifrar');
+BtnCifrar.addEventListener('click', function () {
+  const NombreUsuaria = document.getElementById('NomUsuaria').value;
+  console.log(NombreUsuaria)
+  const ObtenerInfoMensaje = document.getElementById('MensajeUsuaria').value;
+  const Offset = document.getElementById('offset').value;
+  console.log(Offset)
+  const MsjeCifrado = cipher.encode(Offset, ObtenerInfoMensaje);
+  console.log(MsjeCifrado)
+  const result = document.getElementById('MensajeCifrado')
+  console.log(result)
+  result.value = MsjeCifrado;
+})
+// DECIFRADO //
+const BtnDescifrar = document.getElementById("BtnDescifrar")
+BtnDescifrar.addEventListener('click', function () {
+  const ObtenerInfoMensaje2 = document.getElementById('MensajeCifrado').value;
+  const Offset = document.getElementById('offset').value;
+  console.log(Offset)
+  const MsjeDecifrado = cipher.decode(Offset, ObtenerInfoMensaje2);
+  console.log(MsjeDecifrado)
+  const result2 = document.getElementById('MensajeDecifrado')
+  console.log(result2)
+  result2.value = MsjeDecifrado;
+});
 
-console.log(cipher);
